36299fb57f7b9aee510ddf7751ec6741ce8fe767
ScaldingMC <scaldingmc@gmail.com>
2016-08-03 07:28:58 -0700

    Configurable Permissions & Added More

--- src/me.scalding/essentials/essentialsmanager/utils/Permission.java
+++ src/me.scalding/essentials/essentialsmanager/utils/Permission.java
@@ -1,29 +1,41 @@
 package essentials.essentialsmanager.utils;
 
+import essentials.Essentials;
+
 public enum Permission {
 
-    COMMAND_LIGHTING("lightning"),
+    COMMAND_LIGHTING(p("lightning")),
 
-    COMMAND_FLY("fly"),
+    COMMAND_FLY(p("fly")),
 
-    COMMAND_FLY_OTHER("fly.other"),
+    COMMAND_FLY_OTHER(p("fly-other")),
 
-    COMMAND_FEED("feed"),
+    COMMAND_FEED(p("feed")),
 
-    COMMAND_FEED_OTHER("feed.other"),
+    COMMAND_FEED_OTHER(p("feed-other")),
 
-    COMMAND_HEAL("heal"),
+    COMMAND_HEAL(p("heal")),
 
-    COMMAND_HEAL_OTHER("heal.other");
+    COMMAND_HEAL_OTHER(p("heal-other")),
+
+    WARP_LIST(p("warp-list")),
+
+    WARP(p("warp")),
+
+    SETWARP(p("set-warp"));
 
     private String permission;
 
     Permission(String permission) {
-        this.permission = "mage.use." + permission;
+        this.permission = p("base-permission-commands") + "." + permission;
 
     }
 
     public org.bukkit.permissions.Permission getPermission() {
         return new org.bukkit.permissions.Permission(permission);
+    }
+
+    public static String p(String s) {
+        return Essentials.getInstance().getConfig().getString("permissions." + s);
     }
 }
